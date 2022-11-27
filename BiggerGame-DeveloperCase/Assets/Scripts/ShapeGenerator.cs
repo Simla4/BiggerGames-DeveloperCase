4bc13ab738b528a5be3eff29c9adf0e620d5de31
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-11-28 02:10:45 +0300

    Procedural creation of shapes to complement the grid

--- BiggerGame-DeveloperCase/Assets/Scripts/ShapeGenerator.cs
+++ BiggerGame-DeveloperCase/Assets/Scripts/ShapeGenerator.cs
@@ -8,10 +8,9 @@ public class ShapeGenerator : MonoBehaviour
 {
     #region Variables
 
-    [SerializeField] private Rigidbody2D rb;
-
-    private float posX;
-    private float posY;
+    [SerializeField] private List<SlicibleObject> slicibleObjectsList = new List<SlicibleObject>();
+    [SerializeField] private List<LevelData> levelDataList;
+    [SerializeField] private List<Transform> newParent;
 
     #endregion
 
@@ -19,28 +18,69 @@ public class ShapeGenerator : MonoBehaviour
 
     private void Start()
     {
-        var rndX = Random.Range(0, 7);
-        posX = rndX / 2;
-
-        var rndY = Random.Range(0, 7);
-        posY = rndY / 2;
-
-        transform.localPosition = new Vector3(posX, posY, 0);
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Slicable"))
-        {
-            Debug.Log(col.gameObject.name);
-        }
+        CreateShape();
     }
 
     #endregion
 
     #region Other Methods
 
+    private void CreateShape()
+    {
+        var minTriangleCount = levelDataList[0].minTriangleCount;
+        var shapeCount = levelDataList[0].shapeCount;
 
+        int i = 0;
+
+        while (i < (shapeCount - 1))
+        {
+            var selectedTriangleIndex = Random.Range(0, slicibleObjectsList.Count);
+            var selectedTriangle = slicibleObjectsList[selectedTriangleIndex];
+
+            if (selectedTriangle.isElected == false)
+            {
+                slicibleObjectsList[selectedTriangleIndex].transform.parent = newParent[0].parent;
+                selectedTriangle.isElected = true;
+            
+            
+                var rnd = Random.Range(0, (36 - minTriangleCount));
+                var shapeTriangleCount = minTriangleCount + rnd;
+
+                i++;
+
+                var newParentIndex = 0;
+
+                int j = 0;
+                while (j < shapeTriangleCount)
+                {
+                    var neighborObjectsCount = selectedTriangle.neighboringObjects.Count;
+                
+                    var newTriangleIndex = Random.Range(0, neighborObjectsCount);
+                    var newTriangle = selectedTriangle.neighboringObjects[newTriangleIndex];
+
+                    if (newTriangle.isElected == false)
+                    {
+                        newTriangle.transform.parent = newParent[newParentIndex];
+                        newTriangle.isElected = true;
+
+                        selectedTriangle = newTriangle;
+
+                        slicibleObjectsList.Remove(selectedTriangle);
+
+                        j++;
+                    }
+                }
+
+                newParentIndex++;
+            }
+            
+        }
+
+        for (int k = 0; i < slicibleObjectsList.Count; i++)
+        {
+            slicibleObjectsList[k].transform.parent = newParent[newParent.Count - 1];
+        }
+    }
 
     #endregion
 }
